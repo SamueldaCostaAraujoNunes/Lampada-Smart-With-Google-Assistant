c2fbf9c546cf9e79d413c8bf3edc0f6b8972bd24
Samuel da Costa Araujo Nunes <samueldacostaaraujonunes@gmail.com>
2020-08-25 14:02:21 -0300

    first commit

---
+++ lampada-smart.ino
@@ -0,0 +1,160 @@
+/*
+ Version 0.1 - March 17 2018
+*/ 
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WebSocketsClient.h> //  https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries
+#include <ArduinoJson.h> // https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries
+#include <StreamString.h>
+
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+WiFiClient client;
+
+#define MyApiKey "" // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
+#define MySSID "" // TODO: Change to your Wifi network SSID
+#define MyWifiPassword "" // TODO: Change to your Wifi network password
+
+#define HEARTBEAT_INTERVAL 300000 // 5 Minutes 
+
+#define interruptorPin 12   //Pino D6 do NodeMCU
+#define relePin 15  //Pino D8 do NodeMCU
+
+bool interruptor = 0;
+bool verificador = 0;
+float intensidade = 1.0;
+string lampId = "5e73b40a372cfa288b0e5f9e"
+
+uint64_t heartbeatTimestamp = 0;
+bool isConnected = false;
+
+ 
+void turnOn(String deviceId) {
+  if (deviceId == lampId) // Device ID of first device
+  {  
+    Serial.println("Lampada : True");
+    digitalWrite(relePin, HIGH);
+  }   
+}
+
+void turnOff(String deviceId) {
+   if (deviceId == lampId) // Device ID of first device
+   {  
+     Serial.println("Lampada : False");
+     digitalWrite(relePin, LOW);
+   }
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      isConnected = false;    
+      Serial.printf("[WSc] Webservice disconnected from sinric.com!\n");
+      break;
+    case WStype_CONNECTED: {
+      isConnected = true;
+      Serial.printf("[WSc] Service connected to sinric.com at url: %s\n", payload);
+      Serial.printf("Waiting for commands from sinric.com ...\n");        
+      }
+      break;
+    case WStype_TEXT: {
+        Serial.printf("[WSc] get text: %s\n", payload);
+
+#if ARDUINOJSON_VERSION_MAJOR == 5
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject((char*)payload);
+#endif
+#if ARDUINOJSON_VERSION_MAJOR == 6        
+        DynamicJsonDocument json(1024);
+        deserializeJson(json, (char*) payload);      
+#endif        
+        String deviceId = json ["deviceId"];     
+        String action = json ["action"];
+        
+        if(action == "action.devices.commands.OnOff") { // Switch 
+            String value = json ["value"]["on"];
+            Serial.println(value); 
+            
+            if(value == "true") {
+                turnOn(deviceId);
+            } else {
+                turnOff(deviceId);
+            }
+        }
+        else if (action == "test") {
+            Serial.println("[WSc] received test command from sinric.com");
+        }
+      }
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      break;
+    default: break;
+  }
+}
+
+void interruptorAvaliable() {
+  interruptor = digitalRead(interruptorPin);
+  if (verificador != interruptor) {
+    digitalWrite(relePin, !digitalRead(relePin));
+    Serial.print("Lampada : ");
+    bool statusRele = (bool)digitalRead(relePin);
+    Serial.println(statusRele);
+    verificador = interruptor;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(relePin, OUTPUT);
+  pinMode(interruptorPin, INPUT);
+
+  interruptor = digitalRead(interruptorPin);
+  
+  WiFiMulti.addAP(MySSID, MyWifiPassword);
+  Serial.println();
+  Serial.print("Connecting to Wifi: ");
+  Serial.println(MySSID);  
+
+  // Waiting for Wifi connect
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    interruptorAvaliable();
+  }
+  if(WiFiMulti.run() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.print("WiFi connected. ");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  // server address, port and URL
+  webSocket.begin("iot.sinric.com", 80, "/"); //"iot.sinric.com", 80
+
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setAuthorization("apikey", MyApiKey);
+  
+  // try again every 5000ms if connection has failed
+  webSocket.setReconnectInterval(5000);   // If you see 'class WebSocketsClient' has no member named 'setReconnectInterval' error update arduinoWebSockets
+}
+
+void loop() {
+  webSocket.loop();
+  
+  if(isConnected) {
+      uint64_t now = millis();
+      
+      // Send heartbeat in order to avoid disconnections during ISP resetting IPs over night. Thanks @MacSass
+      if((now - heartbeatTimestamp) > HEARTBEAT_INTERVAL) {
+          heartbeatTimestamp = now;
+          webSocket.sendTXT("H");          
+      }
+  }
+  interruptorAvaliable();
+}
+ 
